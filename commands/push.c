a91b6563cd4916a5a7a3c843a352f878dec8ec38
Izaro Perez <izperez@c3r2s3.42urduliz.com>
2024-03-27 12:12:59 +0100

    11.commit: push.c

--- commands/push.c
+++ commands/push.c
@@ -6,7 +6,7 @@
 /*   By: izperez <izperez@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/25 12:42:55 by izperez           #+#    #+#             */
-/*   Updated: 2024/03/26 13:25:55 by izperez          ###   ########.fr       */
+/*   Updated: 2024/03/27 12:11:31 by izperez          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,18 +23,23 @@ void	pa(t_psl *a, t_psl *b)
 		return ;
 	temp = b->first;
 	b->first = b->first->next;
-	if (temp->prev != NULL)
-		temp->prev->next = b->first;
-	if (b->first != NULL)
-		b->first->prev = temp->prev;
-	temp->next = a->first;
-	if (a->first != NULL)
-		a->first->prev = temp;
-	a->first = temp;
-	if (a->last != NULL)
-		a->last->next = temp;
+	b->first->prev = b->last;
+	b->last->next = b->first;
+	if (a->first == NULL)
+	{
+		a->first = temp;
+		b->last = temp;
+		temp->next = temp;
+		temp->prev = temp;
+	}
 	else
-		a->last = temp;
+	{
+		temp->next = a->first;
+		temp->prev = a->last;
+		a->first->prev = temp;
+		a->last->next = temp;
+		a->first = temp;
+	}
 }
 
 /* push b: Takes the first element from the stack_a
@@ -48,16 +53,21 @@ void	pb(t_psl *a, t_psl *b)
 		return ;
 	temp = a->first;
 	a->first = a->first->next;
-	if (temp->prev != NULL)
-		temp->prev->next = a->first;
-	if (a->first != NULL)
-		a->first->prev = temp->prev;
-	temp->next = b->first;
-	if (b->first != NULL)
-		b->first->prev = temp;
-	b->first = temp;
-	if (b->last != NULL)
-		b->last->next = temp;
-	else
+	a->first->prev = a->last;
+	a->last->next = a->first;
+	if (b->first == NULL)
+	{
+		b->first = temp;
 		b->last = temp;
+		temp->next = temp;
+		temp->prev = temp;
+	}
+	else
+	{
+		temp->next = b->first;
+		temp->prev = b->last;
+		b->first->prev = temp;
+		b->last->next = temp;
+		b->first = temp;
+	}
 }
